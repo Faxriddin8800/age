7a5106b1006a9a169bf3b4f487fdddc484dc1991
faxriddin <faxa@mail.ru>
2022-10-04 22:23:57 +0500

    age

---
+++ main.js
@@ -0,0 +1,13 @@
+var age = +prompt('YOSHINGIZNI KIRITING')
+
+if(age  +  0<=18){
+alert("Yoshsiz. O'qishingiz kerak")
+}else if(age <= 50 ){
+    alert("Ishlashingiz kerak")
+}else if(age <= 59){
+    alert("Yaqinda pensiyaga chiqasiz")
+}else if(age <= 150){
+    alert("Pensionerga o'xshaysiz, hali tirik bo'lsangiz")
+}else{
+    alert("Nimadir noto'g'ri ketib qoldi")
+}
